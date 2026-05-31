82328e8bb7fa60db22316e58cf8b8be241ad0f01
henri-32 <henrikirchhoff@gmail.com>
2026-05-31 14:29:42 +0200

    tmux startup for coding session added

---
+++ setup_scripts/tmux_coding.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+set -euo pipefail 
+
+tmux new -d -s code -n git 
+tmux new-window -t code:1 -n code 
+tmux new-window -t code:2 -n build
+tmux new-window -t code:3 -n config
+
+tmux new -d -s code2 -t code 
+
+tmux send-keys -t code:0 "git fetch && git status" C-m 
+
+tmux send-keys -t code:1 "nvim ." C-m 
+
+tmux send-keys -t code:3 " cd ~/Softwareprojekte/myconfig_git && nvim ." C-m 
+
+
+tmux attach -t code2 
