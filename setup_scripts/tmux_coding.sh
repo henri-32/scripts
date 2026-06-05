b956c4b6a00ad3e38445f40081eb38f865871b64
henri-32 <henrikirchhoff@gmail.com>
2026-06-05 02:05:49 +0200

    Window2 switched to logs

--- setup_scripts/tmux_coding.sh
+++ setup_scripts/tmux_coding.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 tmux new -d -s code -n git 
 tmux new-window -t code:1 -n code 
 tmux new-window -t code:2 -n build
-tmux new-window -t code:3 -n generalConfig
+tmux new-window -t code:3 -n logs
 
 tmux new -d -s code2 -t code 
 
@@ -12,7 +12,7 @@ tmux send-keys -t code:0 "git fetch && git status" C-m
 
 tmux send-keys -t code:1 "nvim-server ." C-m 
 
-tmux send-keys -t code:3 " cd ~/Softwareprojekte/myconfig_git && nvim ." C-m 
+tmux send-keys -t code:3 "nvim .logs" C-m 
 
 
 tmux attach -t code2 
